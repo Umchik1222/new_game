d4bd06802e3f02947a6a7b1e34cc0f28fe2cbbee
Sadovoi Grigorii <368748@niuitmo.ru>
2023-11-30 10:26:26 +0300

    test

---
+++ engine/build/CMakeFiles/mygame.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mygame.dir/engine/src/glad.c.obj"
+  "CMakeFiles/mygame.dir/engine/src/glad.c.obj.d"
+  "CMakeFiles/mygame.dir/engine/src/main.c.obj"
+  "CMakeFiles/mygame.dir/engine/src/main.c.obj.d"
+  "libmygame.dll.a"
+  "mygame.exe"
+  "mygame.exe.manifest"
+  "mygame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mygame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
